c9460fecbfb4890c5aa5daed0c0b012677bdd7fc
davidspicks <58148382+davidspicks@users.noreply.github.com>
2020-01-19 10:21:09 -0800

    Add React component picks.js

---
+++ picks.js
@@ -0,0 +1,51 @@
+﻿const e = React.createElement;
+//render a single event
+function listEvents(event) {
+
+    function badges(badge) {
+        return e('img', {
+            src: badge.image
+            , className: 'badge'
+            , alt: badge.name
+            , style: { width: '18px', height: '18px' }
+        })
+    }
+
+    function listlinks(link) {
+        return e("span", {
+            key: link.id
+        }, e("a", {
+            className: "InfoLink",
+            href: link.URL,
+            target: "_blank",
+            rel: "noopener noreferrer"
+        }, link.name));
+    }
+
+    return e('div', { id: 'events' }, [
+        e('h3', null, e('a', { className: 'title', href: event.titleURL, target: '_blank' }, event.title))
+        , e('span', { className: 'badges' }, event.badges.map(badges))
+        , e("span", { className: "description", dangerouslySetInnerHTML: { __html: event.description } })
+        , e('span', { className: 'info_links' }, event.links.map(listlinks))
+        //		, e('span', { className: "info_links" }, event.links.map(listLinks))
+        //		, e('span', { className: 'sponsor' }, sponsor(event))
+        , e('a', { className: 'sponsor_link', href: event.sponsorURL, target: '_blank' }, event.sponsor)
+    ]);
+}
+
+class Picks extends React.Component {
+    render() {
+        //        return React.createElement('div', null, `Hello ${this.props.toWhat}`);
+        //return React.createElement('div', null, `Hello ${picksObj.Heading}`);
+        return e('div', { id: 'post' }, [
+            e('h2', { id: 'post_heading' }, (picksObj.Heading))
+            , e('div', { id: 'post_events' }, (picksObj.Events.map(listEvents)))
+        ]);
+    }
+}
+
+ReactDOM.render(
+    //React.createElement(Hello, { toWhat: 'World' }, null),
+    React.createElement(Picks, null, null),
+    document.getElementById('picks')
+);
