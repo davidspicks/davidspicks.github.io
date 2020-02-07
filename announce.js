db6801f79b2a388974443a7d8502692cbc261227
davidspicks <58148382+davidspicks@users.noreply.github.com>
2020-02-07 14:26:43 -0800

    Add files via upload

--- announce.js
+++ announce.js
@@ -1,1 +1,9 @@
-document.getElementById("stage-01-announce").innerHTML = "Stage 01: PicksWeb Nocompile build 010";
+buildtitle = "picks-web-react-nocompile";
+buildnumber = "010";
+
+if (document.getElementById("stage-01-announce")) {
+  document.getElementById("stage-01-announce").innerHTML = buildtitle + "; build " + buildnumber;
+} else if (document.getElementById("base-announce"))
+{
+  document.getElementById("base-announce").innerHTML = buildtitle + "; build " + buildnumber;
+}
