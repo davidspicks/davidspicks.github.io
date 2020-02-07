baffdce6358a27a22b35babb2bcb1eb194b9aaf6
davidspicks <58148382+davidspicks@users.noreply.github.com>
2020-02-07 11:52:03 -0800

    PicksWeb nocompile build 010
    davidspicks.com content with 
    1) react no-compile app implemented as <script> tags in index.html to render content from  picks_content.js 
    2) react no-compile code in filter_button.js to render show/hide filter checkbox
    3) picks favicon

---
+++ stage-01/filter_button.js
@@ -0,0 +1,193 @@
+﻿'use strict';
+
+var tempBadges = picksObj.Badges
+//console.log(tempBadges.length)
+
+function handleBadgeClick(badge) {
+    console.log(badge.name)
+//    console.log(filters.length)
+//   const tempBadges = [...picksObj.Badges]
+ //   const tempBadge = tempBadges.find(tempBadge => tempBadge.name === badge.name)
+/*    var tempFilters = [...filters]
+    var tempFilter = tempFilters.find(tempFilter => tempFilter.name === badge.name)
+    if (tempfilter) {
+        //toggle checked property of existing filter
+        tempFilter.checked = !tempFilter.checked
+    } else {
+        //add new badge to filter list
+        tempFilters = [...filters, {name: tempFilter.name, checked: tempFilter.checked}]
+    }
+    setFilters(tempFilters)
+    */
+}
+
+//const e = React.createElement;
+//returns a single badge filter checkbox
+function badgeFilter(badge) {
+    return e('label', { key: badge.id },
+        e("input", {
+        type: "checkbox",
+        defaultChecked: false,
+//        onChange: handleBadgeClick
+//        onChange: () => console.log(badge.name)
+            onChange: () => handleBadgeClick(badge)
+        }), badge.name.split(' ')[0])
+}
+
+function handleDateRange() {
+    //TODO: get selected value using React
+    var sel = document.getElementById('select-date-range');
+    console.log(sel.value)
+}
+
+//render a single event
+var index = -1
+function listEvents(event) {
+
+    function badges(badge) {
+
+        return e('img', {
+            key: badge.id
+            , src: badge.image
+            , className: 'badge'
+            , alt: badge.name
+            , style: { width: '18px', height: '18px' }
+        })
+    }
+
+    function listlinks(link) {
+        return e("span", {
+            key: link.id
+        }, e("a", {
+            className: "InfoLink",
+            href: link.URL,
+            target: "_blank",
+            rel: "noopener noreferrer"
+        }, link.name));
+    }
+
+    //TODO: implement surrounding () with pure React
+    if (event.sponsor === "") {
+        var strSponsorHtml = ""
+    } else {
+        var strSponsorHtml =
+            "(<a href=\""
+            + event.sponsorURL
+            + "\" target=\"_blank\">"
+            + event.sponsor
+            + "</a>)"
+    }
+
+    function sponsor() {
+        if (event.sponsor === "")
+            return null
+        return e('a', { href: event.sponsorURL }, '(', event.sponsor, ')')
+    }
+
+    var dow = new Array();
+    dow[1] = "MON";
+    dow[2] = "TUE";
+    dow[3] = "WED";
+    dow[4] = "THU";
+    dow[5] = "FRI";
+    dow[6] = "SAT";
+    dow[0] = "SUN";
+
+    var month = new Array();
+    month[0] = "Jan";
+    month[1] = "Feb";
+    month[2] = "Mar";
+    month[3] = "Apr";
+    month[4] = "May";
+    month[5] = "Jun";
+    month[6] = "Jul";
+    month[7] = "Aug";
+    month[8] = "Sep";
+    month[9] = "Oct";
+    month[10] = "Nov";
+    month[11] = "Dec";
+
+    index++
+
+    // format event start/end dates
+    var Smsec = Date.parse(event.startDate);
+    var Sd = new Date(Smsec);
+    var Emsec = Date.parse(event.endDate);
+    var Ed = new Date(Emsec);
+    if (event.startDate === event.endDate)
+        var strDow = dow[Sd.getDay()] + ', ' + month[Sd.getMonth()] + ' ' + Sd.getDate()
+    else
+        var strDow = dow[Sd.getDay()] + ', ' + month[Sd.getMonth()] + ' ' + Sd.getDate()
+            + ' to ' + dow[Ed.getDay()] + ', ' + month[Ed.getMonth()] + ' ' + Ed.getDate()
+
+    return e('p', { className: 'event', id: 'event_' + index, key: event.id + index }, [
+        e('strong', { className: 'title' }
+            , e('a', { className: 'title_link', href: event.titleURL, target: '_blank' }, event.title)
+        )
+        , e('span', { className: 'badges' }, event.badges.map(badges))
+        , e("br", null)
+        , e("strong", { className: "dates" }, strDow)
+        , e("br", null)
+        , e("span", { className: "description", dangerouslySetInnerHTML: { __html: event.description } })
+        , e('span', { className: 'info_links' }, event.links.map(listlinks))
+        , e("br", null)
+        , e("span", { className: "sponsor", dangerouslySetInnerHTML: { __html: strSponsorHtml } })
+        //        , e("span", { className: "sponsor" }, [sponsor] )
+    ]);
+}
+
+class FilterButton extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { show: false };
+        this.badges = picksObj.Badges
+    }   
+    render() {
+        if (this.state.show) {
+            return e('p', null,
+                e('button',
+                    { onClick: () => this.setState({ show: false }) },
+                    'Hide Filters'),
+                e("br", null),
+                e('span', null, picksObj.Badges.map(badgeFilter)),
+                e("br", null),
+                e("label", null, "Show events for: ",
+                    e("select", {
+                        id: "select-date-range",
+                        //value: optionsState,
+                        onChange: handleDateRange
+                    },
+                        e("option", { value: "1" }, "Next Week"),
+                        e("option", { value: "2" }, "2 Weeks"),
+                        e("option", { value: "3" }, "3 Weeks"),
+                        e("option", { value: "4" }, "Next Month")
+                    )
+                ),
+                e('div', { id: 'post' }
+                    , e('h2', { id: 'post_heading' }, (picksObj.Heading))
+                    , (picksObj.Events.map(listEvents))
+                )
+            )
+        }
+        if (!this.state.show) { 
+        return e('p', null,
+            e('button',
+                { onClick: () => this.setState({ show: true }) },
+                'Show Filters'),
+            e('div', { id: 'post' }
+                , e('h2', { id: 'post_heading' }, (picksObj.Heading))
+                , (picksObj.Events.map(listEvents)) //SCRIPT5009: SCRIPT5009: 'listEvents' is not defined - filter_button.js(75, 9)
+            )
+        )
+        }
+    }
+}
+
+const domContainer = document.querySelector('#filter_button_container');
+ReactDOM.render(e(FilterButton), domContainer);
+
+/* Sample HTML
+ 		<!-- Load React component. -->
+		<div id="filter_button_container"></div>
+		<script src="filter_button.js"></script>
+ */
---
+++ stage-01/picks_content.js
@@ -0,0 +1,287 @@
+var picksObj = {
+  "Heading": "January 24-January 26, 2020",
+  "fromDate": "2020-01-24T00:00:00",
+  "toDate": "2020-01-26T23:59:59.64",
+  "Events": [
+    {
+      "id": "eZAjL1P/Kkyimth6FUs2kA==",
+      "startDate": "2020-01-25T00:00:00",
+      "endDate": "2020-01-25T00:00:00",
+      "title": "Chinese New Year",
+      "titleURL": "https://www.bing.com/search?q=chinese%20new%20year&qs=n&form=QBRE&sp=-1&pq=chinese%20new%20year&sc=8-16&sk=&cvid=468F5050E19B4BF6A4C689C398A2DBD9",
+      "badges": [],
+      "dow": "SAT",
+      "description": "Chinese cultural celebration of the year of the Rat.",
+      "links": [
+        {
+          "id": "9TGrOvVLRk+I8dlifr4QYQ==",
+          "name": "Find Celebrations",
+          "URL": "https://www.bing.com/search?q=Seattle+Lunar+New+Year+Celebrations&src=IE-SearchBox&FORM=IESR3A&pc=EUPP_"
+        }
+      ],
+      "sponsor": "",
+      "sponsorURL": ""
+    },
+    {
+      "id": "OAA4S5G7fUy0zhYo8xZ+8A==",
+      "startDate": "2020-01-24T00:00:00",
+      "endDate": "2020-02-01T00:00:00",
+      "title": "Seattle Boat Show",
+      "titleURL": "http://www.seattleboatshow.com",
+      "badges": [],
+      "dow": "FRI-FEB 01",
+      "description": "Display and sale of all kinds of watercraft and seminars at the <A HREF=\"http://www.centurylinkfield.com/\" TARGET=\"_blank\">CenturyLink Field Event Center</A> (<A HREF=\"https://binged.it/2SDjz4h\" TARGET=\"_blank\">Map</A>) with a free shuttle to a second location at ",
+      "links": [
+        {
+          "id": "muOSuk8CCEiwBdNx4vS5QQ==",
+          "name": "Chandler's Cove",
+          "URL": "https://binged.it/39sxRdY"
+        }
+      ],
+      "sponsor": "www.seattleboatshow.com",
+      "sponsorURL": "http://www.seattleboatshow.com"
+    },
+    {
+      "id": "upO493r7pkyAM+emqbKy7w==",
+      "startDate": "2020-01-24T00:00:00",
+      "endDate": "2020-01-26T00:00:00",
+      "title": "Northwest Remodeling Expo",
+      "titleURL": "http://www.homeshowcenter.com/Visitors/HomeShow.aspx?show=seattle",
+      "badges": [],
+      "dow": "FRI-SUN",
+      "description": "Experts showcase everything for the home including cabinetry, flooring, additions, basements, energy efficiency and more at the <A HREF=\"https://wscc.com/\" TARGET=\"_blank\">Washington State Convention Center</A> <A HREF=\"https://binged.it/2SCKfBX\" TARGET=\"_blank\">Map</A>",
+      "links": [
+        {
+          "id": "BKUsgZ/o90OdsWiMhESeLw==",
+          "name": "Directions and Parking",
+          "URL": "http://www.wscc.com/parking-directions"
+        }
+      ],
+      "sponsor": "www.homeshowcenter.com",
+      "sponsorURL": "http://www.homeshowcenter.com"
+    },
+    {
+      "id": "cf9qRuvqxEy20mYHEPqE9A==",
+      "startDate": "2020-01-25T00:00:00",
+      "endDate": "2020-01-25T00:00:00",
+      "title": "Skagit Eagle Festival Week 4",
+      "titleURL": "https://concrete-wa.com/skagit-eagle-festival/",
+      "badges": [
+        {
+          "id": "jSlp8egcd0CecMPQrwxE4A==",
+          "name": "Free Admission",
+          "tooltip": "Gate admission is free, additional items may be available for purchase inside",
+          "image": "https://davidspicks.files.wordpress.com/2019/12/dollarx.gif",
+          "order": null
+        },
+        {
+          "id": "oFJDFSVyPkm2WihnvEvfUQ==",
+          "name": "Kid Friendly",
+          "tooltip": "All ages welcome, with features appealing to younger children",
+          "image": "https://davidspicks.files.wordpress.com/2019/12/teddy.jpg",
+          "order": null
+        }
+      ],
+      "dow": "SAT",
+      "description": "Guided walks, photography workshops, arts & crafts sales, tours, raft trips, dance performance and music. At the <A HREF=\"https://skagiteagle.org\" TARGET=\"_blank\">Skagit River Bald Eagle Interpretive Center</A> in Concrete, WA about 2 hrs north of Seattle. <A HREF=\"http://binged.it/wg62sR\" TARGET=\"_blank\">Map</A>",
+      "links": [],
+      "sponsor": "concrete-wa.com",
+      "sponsorURL": "http://concrete-wa.com"
+    },
+    {
+      "id": "cc1EFYSu30ipbDoZBzMPGg==",
+      "startDate": "2020-01-26T00:00:00",
+      "endDate": "2020-01-27T00:00:00",
+      "title": "Northwest Model Hobby Expo",
+      "titleURL": "http://www.nwmhe.com/",
+      "badges": [],
+      "dow": "SUN-MON",
+      "description": "Hobby swap meet, vendors and judged model competition at the <A HREF=\"http://www.evergreenfair.org/\" TARGET=\"_blank\">Evergreen State Fairgrounds</A> in Monroe, WA <A HREF=\"https://binged.it/2SGBo2g\" TARGET=\"_blank\">Map</A>",
+      "links": [
+        {
+          "id": "VQKukM+6hkeXAGH+muKXBg==",
+          "name": "Directions",
+          "URL": "http://www.evergreenfair.org/directions.asp\n"
+        }
+      ],
+      "sponsor": "www.nwmhe.com",
+      "sponsorURL": "http://www.nwmhe.com"
+    },
+    {
+      "id": "M5/52digeUiB0+USGEwsWg==",
+      "startDate": "2020-01-22T00:00:00",
+      "endDate": "2020-01-26T00:00:00",
+      "title": "Washington Sportsman's Show",
+      "titleURL": "https://www.thesportshows.com/shows/washington/",
+      "badges": [
+        {
+          "id": "xJtd4qzDDEKtLrduYCy2Iw==",
+          "name": "Kid Friendly",
+          "tooltip": "All ages welcome, with features appealing to younger children",
+          "image": "https://davidspicks.files.wordpress.com/2019/12/teddy.jpg",
+          "order": null
+        }
+      ],
+      "dow": "WED-SUN",
+      "description": "Fishing, hunting, camping equipment and vendors plus kids' activities and seminars at the <A HREF=\"https://www.thefair.com/\" TARGET=\"_blank\">Washington State Fair and Events Center</A> in Puyallup <A HREF=\"http://binged.it/tFZLl1\" TARGET=\"_blank\">Map</A>",
+      "links": [],
+      "sponsor": "www.otshows.com",
+      "sponsorURL": "http://www.otshows.com"
+    },
+    {
+      "id": "hqT5Ge/WMEudBbex0JTlDA==",
+      "startDate": "2020-01-25T00:00:00",
+      "endDate": "2020-01-25T00:00:00",
+      "title": "Belgian Fest Beer Tasting",
+      "titleURL": "http://www.washingtonbeer.com/belgianfest/",
+      "badges": [
+        {
+          "id": "2uPvlK+KoUuvKn1Hm6/bYQ==",
+          "name": "Beer Garden",
+          "tooltip": "Beer and wine available for purchase",
+          "image": "https://davidspicks.files.wordpress.com/2019/12/mug.gif",
+          "order": null
+        }
+      ],
+      "dow": "SAT",
+      "description": "<STRONG>SAT</STRONG> Washington breweries pouring 100+ Belgian-style beers in the <A HREF=\"http://www.seattlecenter.com/connect/book-an-event/rentals/facilities/fisher-pavilion\" TARGET=\"_blank\">Seattle Center Fisher Pavilion</A>",
+      "links": [
+        {
+          "id": "+sQYIDXPgUyvGaDxBFqHxA==",
+          "name": "Map",
+          "URL": "http://binged.it/1J5XKkO"
+        }
+      ],
+      "sponsor": "www.washingtonbeer.com",
+      "sponsorURL": "http://www.washingtonbeer.com"
+    },
+    {
+      "id": "yo7VERv0Lk6h0fese6d8wQ==",
+      "startDate": "2020-01-26T00:00:00",
+      "endDate": "2020-01-26T00:00:00",
+      "title": "I Dig Dinos at Burke Museum",
+      "titleURL": "https://www.burkemuseum.org/calendar/i-dig-dinos-flight-and-feathers",
+      "badges": [
+        {
+          "id": "b6JX26oL/kqURADnM+au/Q==",
+          "name": "Kid Friendly",
+          "tooltip": "All ages welcome, with features appealing to younger children",
+          "image": "https://davidspicks.files.wordpress.com/2019/12/teddy.jpg",
+          "order": null
+        }
+      ],
+      "dow": "SUN",
+      "description": "<STRONG>SUN</STRONG> Demonstrations and hands-on activities for budding paleontologists of all ages. Included with admission to the <A HREF=\"https://www.burkemuseum.org/\" TARGET=\"_blank\">Burke Museum</A> at UW. <A HREF=\"http://binged.it/A7X4WG\n\n\" TARGET=\"_blank\">Map</A>",
+      "links": [],
+      "sponsor": "",
+      "sponsorURL": ""
+    },
+    {
+      "id": "S89wbypEsEuIajB9M/0GgQ==",
+      "startDate": "2020-01-25T00:00:00",
+      "endDate": "2020-01-25T00:00:00",
+      "title": "Maker Day: Making Train Cars",
+      "titleURL": "https://mohai.org/event/maker-day-building-board-games/",
+      "badges": [
+        {
+          "id": "rQbgIj46ZkmxSd/1hDlk3Q==",
+          "name": "Kid Friendly",
+          "tooltip": "All ages welcome, with features appealing to younger children",
+          "image": "https://davidspicks.files.wordpress.com/2019/12/teddy.jpg",
+          "order": null
+        }
+      ],
+      "dow": "SAT",
+      "description": "Learn how to assemble, paint, and weather unique model railway components with local artist and model train builder <A href=\"http://www.wtrak.org/\" TARGET = _blank>Thomas Stephenson</A>. Included with admission to the <A HREF=\"https://mohai.org/\" TARGET=\"_blank\">Museum of History and Industry</A> at South Lake Union. <A HREF=\"https://binged.it/2tiQsse\" TARGET=\"_blank\">Map</A>",
+      "links": [],
+      "sponsor": "www.mohai.org",
+      "sponsorURL": "http://www.mohai.org"
+    },
+    {
+      "id": "7/QhTVh8MEOKexqqhZe19A==",
+      "startDate": "2020-01-25T00:00:00",
+      "endDate": "2020-01-25T00:00:00",
+      "title": "Stormy Weather Story Time",
+      "titleURL": "http://depts.washington.edu/hortlib/calendar/story_time.shtml",
+      "badges": [
+        {
+          "id": "VdneO5zz6UqHEtrlTWpInQ==",
+          "name": "Free Admission",
+          "tooltip": "Gate admission is free, additional items may be available for purchase inside",
+          "image": "https://davidspicks.files.wordpress.com/2019/12/dollarx.gif",
+          "order": null
+        },
+        {
+          "id": "HwmD3uVJhUeeFcZcrR9iAA==",
+          "name": "Kid Friendly",
+          "tooltip": "All ages welcome, with features appealing to younger children",
+          "image": "https://davidspicks.files.wordpress.com/2019/12/teddy.jpg",
+          "order": null
+        }
+      ],
+      "dow": "SAT",
+      "description": "<STRONG>SAT</STRONG>  When storms come, how do animals and plants react? Hear a fable and two science-based tales of stormy weather. After the stories, kids can make a winter branch art project. All ages are welcome. 10:30am - 11:30am at the <A HREF=\"https://depts.washington.edu/hortlib/\" TARGET=\"_blank\">Elisabeth C. Miller Library</A> on the UW Campus. <A HREF=\"http://binged.it/xJWEdN\n\n\" TARGET=\"_blank\">Map</A>",
+      "links": [],
+      "sponsor": "depts.washington.edu/hortlib/index.shtml",
+      "sponsorURL": "http://depts.washington.edu/hortlib/index.shtml"
+    },
+    {
+      "id": "jk7Fl0OCHUOi8fV3EEN24Q==",
+      "startDate": "2020-01-25T00:00:00",
+      "endDate": "2020-01-26T00:00:00",
+      "title": "Ursulmas Medieval Faire",
+      "titleURL": "http://aquaterra.antir.org/index.php/events/ursulmas",
+      "badges": [
+        {
+          "id": "f0tEutPEcEKrYxJiFyE1Gw==",
+          "name": "Kid Friendly",
+          "tooltip": "All ages welcome, with features appealing to younger children",
+          "image": "https://davidspicks.files.wordpress.com/2019/12/teddy.jpg",
+          "order": null
+        }
+      ],
+      "dow": "SAT-SUN",
+      "description": "A full weekend of events including combat exhibitions, crafts, Bards, archery, youth activities and merchants at the <A HREF=\"http://www.evergreenfair.org/\" TARGET=\"_blank\">Evergreen State Fairgrounds</A> in Monroe <A HREF=\"https://binged.it/2SGBo2g\" TARGET=\"_blank\">Map</A>",
+      "links": [],
+      "sponsor": "www.aquaterra.antir.sca.org",
+      "sponsorURL": "http://www.aquaterra.antir.sca.org"
+    }
+  ],
+  "Badges": [
+    {
+      "id": "Ic5thbYwPE6jOiI8SeoXpQ==",
+      "name": "Free Admission",
+      "tooltip": "Gate admission is free, additional items may be available for purchase inside",
+      "image": "https://davidspicks.files.wordpress.com/2019/12/dollarx.gif",
+      "order": "1"
+    },
+    {
+      "id": "bfxdiDKnpk6Uo5rgLuMD6g==",
+      "name": "Kid Friendly",
+      "tooltip": "All ages welcome, with features appealing to younger children",
+      "image": "https://davidspicks.files.wordpress.com/2019/12/teddy.jpg",
+      "order": "2"
+    },
+    {
+      "id": "DM3ozH1uUkqrGFbwkq5wgA==",
+      "name": "Beer Garden",
+      "tooltip": "Beer and wine available for purchase",
+      "image": "https://davidspicks.files.wordpress.com/2019/12/mug.gif",
+      "order": "3"
+    },
+    {
+      "id": "dMeMS0j+R0CLZz7/yKPOjw==",
+      "name": "Wine Tasting",
+      "tooltip": "Wine Tasting",
+      "image": "https://davidspicks.files.wordpress.com/2020/01/wine-glass.jpg",
+      "order": "4"
+    },
+    {
+      "id": "QeckRoRBzE2X/3VmXk/OEw==",
+      "name": "Pet Friendly",
+      "tooltip": "Pet Friendly",
+      "image": "https://davidspicks.files.wordpress.com/2020/01/pawprint.png",
+      "order": "5"
+    }
+  ]
+}
