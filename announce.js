e229ed4f3cb1239b40c0435ad7aad6720d49f4df
davidspicks <58148382+davidspicks@users.noreply.github.com>
2020-02-07 22:12:54 -0800

    picks-web-react-nocompile; build 011

--- announce.js
+++ announce.js
@@ -1,9 +1,17 @@
 buildtitle = "picks-web-react-nocompile";
-buildnumber = "010";
+buildnumber = "011";
 
-if (document.getElementById("stage-01-announce")) {
-  document.getElementById("stage-01-announce").innerHTML = buildtitle + "; build " + buildnumber;
+if (document.getElementById("stage-03-announce")) {
+  document.getElementById("stage-03-announce").innerHTML = buildtitle + "; build " + buildnumber;
 } else if (document.getElementById("base-announce"))
 {
   document.getElementById("base-announce").innerHTML = buildtitle + "; build " + buildnumber;
 }
+
+document.getElementById("release-notes").innerHTML = 
+'<ul>'
+  +'<li>react-nocompile in app.js</li>'
+  +'<li>date-range select dropdown</li>'
+  +'<li>ReactPicksWebPOC</li>'
+  +'<li>minimum page content</li>'
++'</ul>';   
