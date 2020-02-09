93764edd9346fd4551929dac082d15d9ed9a63d4
davidspicks <58148382+davidspicks@users.noreply.github.com>
2020-02-09 14:11:57 -0800

    2020-02-09 picksweb-react-nocompile build 012

--- announce.js
+++ announce.js
@@ -1,17 +1,36 @@
 buildtitle = "picks-web-react-nocompile";
-buildnumber = "011";
+buildnumber = "012";
 
-if (document.getElementById("stage-03-announce")) {
-  document.getElementById("stage-03-announce").innerHTML = buildtitle + "; build " + buildnumber;
+if (document.getElementById("stage-04-announce")) {
+  document.getElementById("stage-04-announce").innerHTML = buildtitle+'; Build '+buildnumber;
 } else if (document.getElementById("base-announce"))
 {
-  document.getElementById("base-announce").innerHTML = buildtitle + "; build " + buildnumber;
+  document.getElementById("base-announce").innerHTML =  buildtitle+'; Build '+buildnumber;
 }
 
 document.getElementById("release-notes").innerHTML = 
-'<ul>'
+'<ul>Build '+buildnumber
+  +'<li>css styles</li>'
+  +'<li>top hero image</li>'
+  +'<li>banner text overlays hero image</li>'
+  +'<li>dummy comment button overlays hero image</li>'
+  +'<li>right navbar with dummy content</li>'
+  +'<li>picks content panel with scrollbar</li>'
++'</ul>';  
+
+document.getElementById("build-011").innerHTML = 
+'<ul>Build 011'
   +'<li>react-nocompile in app.js</li>'
   +'<li>date-range select dropdown</li>'
   +'<li>ReactPicksWebPOC</li>'
   +'<li>minimum page content</li>'
-+'</ul>';   
++'</ul>';  
+
+document.getElementById("build-010").innerHTML = 
+'<ul>Build 010'
++'<li>react no-compile app implemented as &#060script> tags in index.html to render </li>content from  picks_content.js </li>'
++'<li>react no-compile code in filter_button.js to render show/hide filter button (filter functionality not implemented)</li>'
++'<li>davidspicks.com content</li>'
++'<li>picks favicon</li>'
+
+
