d88424f79e5cd1e2e7b3e8cff49b14a5095d8151
davidspicks <58148382+davidspicks@users.noreply.github.com>
2020-02-13 09:24:12 -0800

    Delete announce.js

--- stage-05/announce.js
+++
@@ -1,9 +0,0 @@
-buildtitle = "stage-05 build name";
-buildnumber = "nnn";
-
-if (document.getElementById("stage-05-announce")) {
-  document.getElementById("stage-05-announce").innerHTML = buildtitle + "; build " + buildnumber;
-} else if (document.getElementById("base-announce"))
-{
-  document.getElementById("base-announce").innerHTML = buildtitle + "; build " + buildnumber;
-}
