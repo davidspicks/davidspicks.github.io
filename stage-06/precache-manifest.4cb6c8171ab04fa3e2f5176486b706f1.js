1d60623b1eb53b4e4991fa6255d70666bed22c15
davidspicks <58148382+davidspicks@users.noreply.github.com>
2020-03-07 11:14:40 -0800

    react-example built with Ubuntu 18

---
+++ stage-06/precache-manifest.4cb6c8171ab04fa3e2f5176486b706f1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7529d9ace28bacc45d61f10184d3b78",
+    "url": "/stage-06/index.html"
+  },
+  {
+    "revision": "314ff2ab6a8b2c7f20ec",
+    "url": "/stage-06/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "36dd8a03126114cf0815",
+    "url": "/stage-06/static/js/2.c1d12bf4.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/stage-06/static/js/2.c1d12bf4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "314ff2ab6a8b2c7f20ec",
+    "url": "/stage-06/static/js/main.3eb446e6.chunk.js"
+  },
+  {
+    "revision": "5bca76dcbffd11dfa39a",
+    "url": "/stage-06/static/js/runtime-main.9fdaf416.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/stage-06/static/media/logo.5d5d9eef.svg"
+  }
+]);
