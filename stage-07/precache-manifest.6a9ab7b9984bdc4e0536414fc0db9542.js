eb8921dad5aa680d4ff48347150e22fb46be70ec
davidspicks <58148382+davidspicks@users.noreply.github.com>
2020-03-07 16:09:36 -0800

    picksweb-react npm Ubuntu build 013

---
+++ stage-07/precache-manifest.6a9ab7b9984bdc4e0536414fc0db9542.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac040ae45690cceb4feb1472b3759603",
+    "url": "/stage-06/index.html"
+  },
+  {
+    "revision": "87547be8487203384682",
+    "url": "/stage-06/static/js/2.12836f3b.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/stage-06/static/js/2.12836f3b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "686c3bab00cd8b435960",
+    "url": "/stage-06/static/js/main.3d9f065b.chunk.js"
+  },
+  {
+    "revision": "5bca76dcbffd11dfa39a",
+    "url": "/stage-06/static/js/runtime-main.9fdaf416.js"
+  }
+]);
