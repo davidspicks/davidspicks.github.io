c58f9a5cd2a3ec0ef7ec7d9bf6a799e95f5458b3
davidspicks <58148382+davidspicks@users.noreply.github.com>
2020-03-10 12:45:24 -0700

    picksweb-react build 014

---
+++ stage-08/announce.js
@@ -0,0 +1,54 @@
+buildtitle = "picks-web-react";
+buildnumber = "014";
+releasenotes = 
+'<ul>Build '+buildnumber
+  +'<li>top hero and right nav</li>'
++'</ul>';
+
+if (document.getElementById("stage-08-announce")) 
+{
+  document.getElementById("stage-08-announce").innerHTML = buildtitle+'; Build '+buildnumber;
+} 
+else if (document.getElementById("base-announce"))
+{
+  document.getElementById("base-announce").innerHTML = buildtitle;
+  document.getElementById("base-release-notes").innerHTML = releasenotes;
+}
+
+//historic notes from previous builds
+if (document.getElementById("release-notes-build-013")) {
+	document.getElementById("release-notes-build-013").innerHTML = 
+	'<ul>Build 013'
+		  +'<li>built with Ubuntu 18.04.1</li>'
+		  +'<li>react-select controls for daterange and badge filter</li>'
+		  +'<li>jsx code</li>'
+		  +'<li>no css</li>'
+	+'</ul>';  }
+
+if (document.getElementById("release-notes-build-012")) {
+	document.getElementById("release-notes-build-012").innerHTML = 
+	'<ul>Build 012'
+	  +'<li>css styles</li>'
+	  +'<li>top hero image</li>'
+	  +'<li>banner text overlays hero image</li>'
+	  +'<li>dummy comment button overlays hero image</li>'
+	  +'<li>right navbar with dummy content</li>'
+	  +'<li>picks content panel with scrollbar</li>'
+	+'</ul>';  }
+
+if (document.getElementById("release-notes-build-011")) {
+document.getElementById("release-notes-build-011").innerHTML = 
+'<ul>Build 011'
+  +'<li>react-nocompile in app.js</li>'
+  +'<li>date-range select dropdown</li>'
+  +'<li>ReactPicksWebPOC</li>'
+  +'<li>minimum page content</li>'
++'</ul>';  }
+
+if (document.getElementById("release-notes-build-010")) {
+document.getElementById("release-notes-build-010").innerHTML = 
+'<ul>Build 010'
++'<li>react no-compile app implemented as &#060script> tags in index.html to render </li>content from  picks_content.js </li>'
++'<li>react no-compile code in filter_button.js to render show/hide filter button (filter functionality not implemented)</li>'
++'<li>davidspicks.com content</li>'
++'<li>picks favicon</li>'; }
---
+++ stage-08/precache-manifest.97f1e72a3479b5c3aa6446abfa9bbcac.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e45c5494c0286943ca641a4646bb7f0",
+    "url": "/stage-08/index.html"
+  },
+  {
+    "revision": "e045fe9735d364472a8c",
+    "url": "/stage-08/static/css/main.3a6f98d0.chunk.css"
+  },
+  {
+    "revision": "36732ca58b66a14aecfe",
+    "url": "/stage-08/static/js/2.6a1a0bcc.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/stage-08/static/js/2.6a1a0bcc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e045fe9735d364472a8c",
+    "url": "/stage-08/static/js/main.292379f0.chunk.js"
+  },
+  {
+    "revision": "1ac9b340037e3dcd446c",
+    "url": "/stage-08/static/js/runtime-main.719b64c8.js"
+  },
+  {
+    "revision": "19a524169c50d487d6e5b7eca8f334eb",
+    "url": "/stage-08/static/media/Seattle 6952-6 940x198.19a52416.jpg"
+  }
+]);
